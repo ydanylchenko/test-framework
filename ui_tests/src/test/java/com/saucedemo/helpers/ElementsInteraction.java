8a08e8770690a1947837514f2b2a832fbd7aecc5
Yaroslav Danylchenko <danilchenkoy@gmail.com>
2020-03-07 16:51:19 -0500

    Add the required test

--- ui_tests/src/test/java/com/saucedemo/helpers/ElementsInteraction.java
+++ ui_tests/src/test/java/com/saucedemo/helpers/ElementsInteraction.java
@@ -1,35 +1,17 @@
 package com.saucedemo.helpers;
 
-import com.saucedemo.http.HttpRequestFactory;
 import com.saucedemo.selenium.SeleniumConfig;
 import com.saucedemo.selenium.WaitForAjaxCalls;
 import org.openqa.selenium.*;
-import org.openqa.selenium.interactions.Actions;
-import org.openqa.selenium.remote.RemoteWebDriver;
-import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-import java.awt.*;
-import java.awt.datatransfer.Clipboard;
-import java.awt.datatransfer.StringSelection;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Objects;
-import java.util.stream.Collectors;
 
 import static com.saucedemo.CucumberHooks.getDriver;
 import static org.openqa.selenium.support.ui.ExpectedConditions.*;
-import static org.testng.Assert.assertEquals;
-import static org.testng.Assert.assertTrue;
 
 /**
  * Select element from active search results.
  */
 public final class ElementsInteraction {
-    private static final Logger LOG = LoggerFactory.getLogger(ElementsInteraction.class);
     private static final int TIMEOUT = SeleniumConfig.getConfig().getPageLoadingRetriesCount();
 
     private ElementsInteraction() {
@@ -43,190 +25,10 @@ public final class ElementsInteraction {
         getWait().until(elementToBeClickable(elementLocator)).click();
     }
 
-    public static void click(WebElement element) {
-        getWait().until(elementToBeClickable(element)).click();
-    }
-
-    public static void click(By elementLocator, int xOffset, int yOffset) {
-        new Actions(getDriver())
-                .moveToElement(getDriver().findElement(elementLocator), xOffset, yOffset)
-                .click()
-                .build()
-                .perform();
-    }
-
-    public static void markCheckbox(WebElement checkbox, String state) {
-        List<String> states = new ArrayList<>(Arrays.asList("checked", "unchecked"));
-        assertTrue(states.contains(state.toLowerCase()), state + " should be in " + states.toString());
-        boolean expectedSelectedState = state.equalsIgnoreCase("checked");
-        if (expectedSelectedState != checkbox.isSelected()) {
-            new Actions(getDriver()).moveToElement(checkbox, 5, 5).click().perform();
-        }
-        getWait().until(elementSelectionStateToBe(checkbox, expectedSelectedState));
-    }
-
-    public static void markCheckbox(By checkboxElementLocator, String state) {
-        markCheckbox(getWait().until(presenceOfElementLocated(checkboxElementLocator)), state);
-    }
-
-    public static void clear(WebElement element) {
-        element.clear();
-        if (!getText(element).isEmpty()) {
-            int characters = getText(element).length();
-            while (characters > 0) {
-                element.sendKeys(Keys.BACK_SPACE);
-                characters--;
-            }
-        }
-        assertTrue(getText(element) == null || Objects.requireNonNull(getText(element)).isEmpty(),
-                String.format("Field should be empty: '%s'", getText(element)));
-    }
-
-    public static void clear(By inputElementLocator) {
-        verifyElementPresence(inputElementLocator);
-        clear(getDriver().findElement(inputElementLocator));
-    }
-
     public static void sendKeys(By inputElementLocator, String text) {
-        sendKeys(inputElementLocator, text, false);
-    }
-
-    public static void sendKeys(By inputElementLocator, String text, boolean isAutocomplete) {
-        click(inputElementLocator);
-        WebElement input = getDriver().findElement(inputElementLocator);
-        if (isAutocomplete) {
-            char[] chars = text.toCharArray();
-            for (char character : chars) {
-                input.sendKeys(String.valueOf(character));
-                try {
-                    Thread.sleep(25);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-                new WaitForAjaxCalls(getDriver()).checkPendingRequests();
-            }
-            assertEquals(text, getText(input), "Text is typed properly");
-            new WaitForAjaxCalls(getDriver()).checkPendingRequests();
-            input.sendKeys(Keys.ENTER);
-        } else {
-            if (SeleniumConfig.getConfig().getSeleniumBrowserIsGrid()) {
-                String fileUrl = String.join("/", SeleniumConfig.getConfig().getSeleniumGridUrl(),
-                        "clipboard", ((RemoteWebDriver) getDriver()).getSessionId().toString());
-                new HttpRequestFactory().sendRequest(fileUrl, "POST", text, null);
-            } else {
-                StringSelection fieldValueSelection = new StringSelection(text);
-                Clipboard clipboard = Toolkit.getDefaultToolkit().getSystemClipboard();
-                clipboard.setContents(fieldValueSelection, fieldValueSelection);
-            }
-            getDriver().findElement(inputElementLocator).sendKeys(Keys.SHIFT, Keys.INSERT);
-            new WaitForAjaxCalls(getDriver()).checkPendingRequests();
-//        Then check if it equals to the expected value
-            assertEquals(getText(input), text, "Text is typed properly");
-        }
-    }
-
-    /**
-     * Used when Collapse menu element is a button.
-     *
-     * @param menuElementLocator    - Main element
-     * @param subMenuElementLocator - Element from submenu
-     */
-    public static void clickOnMenuOption(By menuElementLocator, By subMenuElementLocator) {
-        WebDriver driver = getDriver();
-//        Hover menu element action
-        Actions hoverMenuAction = new Actions(driver);
-//        Move mouse to the menu element
-        hoverMenuAction.moveToElement(driver.findElement(menuElementLocator));
-//        Perform action
-        hoverMenuAction.build().perform();
-//        Wait for menu to be opened
-        getWait().until(presenceOfElementLocated(subMenuElementLocator));
-//        Select menu option action
-        Actions selectMenuOptionAction = new Actions(driver);
-//        Click on menu option
-        selectMenuOptionAction.click(driver.findElement(subMenuElementLocator));
-//        Perform action
-        selectMenuOptionAction.build().perform();
-//        Select menu option action
-        Actions unHoverMenuOptionAction = new Actions(driver);
-//        Move cursor away from menu
-        unHoverMenuOptionAction.moveByOffset(250, 250);
-//        Perform action
-        unHoverMenuOptionAction.build().perform();
-    }
-
-    public static void selectByVisibleText(By selectElementLocator, String optionText) {
-        verifyElementPresence(selectElementLocator);
-        WebElement selectWebElement = getDriver().findElement(selectElementLocator);
-        Select select = new Select(selectWebElement);
-        select.selectByVisibleText(optionText);
-        assertEquals(optionText, getText(select.getFirstSelectedOption()), "Selected option text");
-    }
-
-    public static String getText(WebElement element) {
-        String text = element.getText();
-        if (!text.isEmpty()) {
-            return element.getText();
-        } else if (element.getAttribute("value") != null) {
-            return element.getAttribute("value");
-        } else if (element.getAttribute("innerText") != null) {
-            return element.getAttribute("innerText");
-        } else {
-            return text;
-        }
-    }
-
-    public static String getText(By elementLocator) {
-        return getText(getWait().until(visibilityOfElementLocated(elementLocator)));
-    }
-
-    /**
-     * highlight with red border element located by locator for one second
-     *
-     * @param elementLocator element locator
-     */
-    private static void highlightElement(By elementLocator) {
-        highlightElement(getDriver().findElement(elementLocator));
-    }
-
-    /**
-     * highlight with red border specified web element for one second
-     *
-     * @param element web element
-     */
-    private static void highlightElement(WebElement element) {
-        ((JavascriptExecutor) getDriver()).executeScript("arguments[0].style.border='3px solid red'", element);
-    }
-
-    public static boolean getAvailability(String availability) {
-        assertTrue(availability.matches("(available|not available)"),
-                availability + "should be 'available' or 'not available'");
-        return availability.equals("available");
-    }
-
-    public static void verifyElementPresence(By elementLocator, String availability) {
-        verifyElementPresence(elementLocator, getAvailability(availability));
-    }
-
-    public static void verifyElementPresence(By elementLocator) {
-        verifyElementPresence(elementLocator, true);
-    }
-
-    public static void verifyElementPresence(By elementLocator, boolean availability) {
-        if (availability) {
-            getWait().until(visibilityOfElementLocated(elementLocator));
-        } else {
-            waitForElementAbsence(elementLocator);
-        }
-    }
-
-    public static void waitForElementAbsence(By elementLocator) {
-        try {
-            getWait().until(numberOfElementsToBe(elementLocator, 0));
-        } catch (WebDriverException e) {
-            highlightElement(elementLocator);
-            throw e;
-        }
+        getWait().until(visibilityOfElementLocated(inputElementLocator)).sendKeys(text);
+        new WaitForAjaxCalls(getDriver()).checkPendingRequests();
+        assertElementValue(inputElementLocator, text);
     }
 
     public static void assertElementText(By elementLocator, String text) {
@@ -237,21 +39,6 @@ public final class ElementsInteraction {
     public static void assertElementValue(By elementLocator, String text) {
         getWait().ignoring(StaleElementReferenceException.class)
                 .until(textToBePresentInElementValue(elementLocator, text));
-    }
-
-    public static int getColumnIndex(String columnName) {
-        By columnNameCell = By.xpath("//thead//td");
-        getWait().ignoring(StaleElementReferenceException.class).until(visibilityOfAllElementsLocatedBy(columnNameCell));
-        String lowerCaseColumnName = columnName.toLowerCase();
-        List<String> cells = getDriver().findElements(columnNameCell).stream()
-                .map(ElementsInteraction::getText)
-                .map(cellText -> Objects.requireNonNull(cellText).trim().toLowerCase())
-                .collect(Collectors.toList());
-        assertTrue(cells.size() > 0, "Column headers should be available on the page");
-        assertTrue(cells.contains(lowerCaseColumnName),
-                String.format("%s column should be in [%s]", lowerCaseColumnName, String.join(", ", cells)));
-//        xpath indexes (in the spots this index will be used) are starting from 1, not 0
-        return cells.indexOf(lowerCaseColumnName) + 1;
     }
 
     public static By getLocator(String elementLocatorTemplate, String... dynamicLocatorParts) {
--- ui_tests/src/test/resources/com/saucedemo/products/purchaseProduct.feature
+++ ui_tests/src/test/resources/com/saucedemo/products/purchaseProduct.feature
@@ -1,5 +1,5 @@
 #Login as standard_user and purchase ALL the T-shirts.
-Feature: Recruiter Sign Up
+Feature: Purchase: One Product
 
   Background:
     Given I open start page
@@ -7,8 +7,8 @@ Feature: Recruiter Sign Up
     And I set 'secret_sauce' as password on Sign in page
     And I click 'Sign In' button on Sign in page
 
-  @recruiters @recruiterSignUp @recruiterEmail
-  Scenario Outline: Purchase <product> product
+  @purchase
+  Scenario Outline: Purchase '<product>' product
     When I click 'ADD TO CART' button on '<product>' product on Products page
     Then Cart contains '1' item on Header
     And I click on cart icon on Header
@@ -35,5 +35,5 @@ Feature: Recruiter Sign Up
       | Sauce Labs Bike Light             | A red light isn't the desired state in testing but it sure helps when riding your bike at night. Water-resistant with 3 lighting modes, 1 AAA battery included.        | 9.99  | 0.80 | 10.79 |
       | Sauce Labs Bolt T-Shirt           | Get your testing superhero on with the Sauce Labs bolt T-shirt. From American Apparel, 100% ringspun combed cotton, heather gray with red bolt.                        | 15.99 | 1.28 | 17.27 |
       | Sauce Labs Fleece Jacket          | It's not every day that you come across a midweight quarter-zip fleece jacket capable of handling everything from a relaxing day outdoors to a busy day at the office. | 49.99 | 4.00 | 53.99 |
-      | Sauce Labs Onesie                 | Rib snap infant onesie for the junior automation engineer in development. Reinforced 3-snap bottom closure, two-needle hemmed sleeved and bottom won't unravel.        | 7.99  | 0.64 | 8.63  |
+      | Sauce Labs Onesie                 | sRib snap infant onesie for the junior automation engineer in development. Reinforced 3-snap bottom closure, two-needle hemmed sleeved and bottom won't unravel.        | 7.99  | 0.64 | 8.63  |
       | Test.allTheThings() T-Shirt (Red) | This classic Sauce Labs t-shirt is perfect to wear when cozying up to your keyboard to automate a few tests. Super-soft and comfy ringspun combed cotton.              | 15.99 | 1.28 | 17.27 |
